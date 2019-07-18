963124d831f00a57cc922d47f6443c176b277c84
Icarous <anglemiku.v@gmail.com>
2019-07-18 13:19:50 +0800

    [update] second

---
+++ second.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+# --------输出
+
+print("Hello world!!!")
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+print('100 + 200 =', 100 + 200)
+
+
+# --------输入
+
+name = input('please enter your name: ')
+print('hello,', name)
+
+# --------练习
+
+number = 1024 * 768
+print("1024 * 768 =", number)

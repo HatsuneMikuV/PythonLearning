5bc94524a5ae3f0541cb226c155d63cadeb182b2
anglemiku <anglemiku.v@gmail.com>
2019-12-17 15:17:32 +0800

    [update] exercise 9

---
+++ 36.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:        36
+   Description :     ^_^ !!!
+   Author :          anglemiku
+   Eamil :           anglemiku.v@gmail.com
+   date:             2019/12/17
+-------------------------------------------------
+   Change Activity:  2019/12/17:
+-------------------------------------------------
+"""
+
+'''
+题目：暂停一秒输出。
+'''
+
+import time
+
+if __name__ == '__main__':
+
+    print '暂停一秒输出1'
+    time.sleep(1)
+    print '暂停一秒输出2'
+    
+    pass

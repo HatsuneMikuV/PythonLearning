06785c31af1781bfc45b90ab65a61b461ae54cd0
anglemiku <anglemiku.v@gmail.com>
2020-01-15 14:34:00 +0800

    [update] exercise 41

---
+++ 68.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:        68
+   Author :          anglemiku
+   Eamil :           anglemiku.v@gmail.com
+   date:             2020/1/15
+-------------------------------------------------
+   Change Activity:  2020/1/15:
+-------------------------------------------------
+"""
+
+'''
+题目：模仿静态变量的用法。
+'''
+
+def varfunc():
+    var = 0
+    print 'var = %d' % var
+    var += 1
+if __name__ == '__main__':
+    for i in range(3):
+        varfunc()
+
+# 类的属性
+# 作为类的一个属性吧
+class Static:
+    StaticVar = 5
+    def varfunc(self):
+        self.StaticVar += 1
+        print self.StaticVar
+
+print Static.StaticVar
+a = Static()
+for i in range(3):
+    a.varfunc()
